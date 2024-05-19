499a790892195f4bd276390ac82e1df1cd8afb7a
David Simmerman <davidsimmermancs@gmail.com>
2024-05-19 15:41:30 -0400

    add mini pairs and mini animate

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -34,4 +34,47 @@ return {
 			})
 		end,
 	},
+	{
+		"echasnovski/mini.animate",
+		opts = function()
+			-- don't use animate when scrolling with the mouse
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set({ "", "i" }, key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
+			local animate = require("mini.animate")
+			return {
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 25, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
+				},
+				cursor = {
+					enable = true,
+					timing = animate.gen_timing.linear({ duration = 25, unit = "total" }),
+				},
+				resize = { enable = false },
+			}
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		opts = {
+			mappings = {
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+			},
+		},
+	},
 }

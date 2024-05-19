e2553eba8cc5e3efdaaf3a85021c4c91b91b7eb0
David Simmerman <davidsimmermancs@gmail.com>
2024-05-18 21:28:01 -0400

    add none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,17 +1,41 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end
+		opts = {
+			ensure_installed = { "stylua", "prettier", "eslint_d", "black" },
+		},
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+
+			for _, tool in ipairs(opts.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "vtsls", "eslint", "graphql", "html", "cssls", "remark_ls", "jsonls", "lemminx", "pyright", "typos_lsp", "grammarly"}
+				ensure_installed = {
+					"lua_ls",
+					"vtsls",
+					"eslint",
+					"graphql",
+					"html",
+					"cssls",
+					"remark_ls",
+					"jsonls",
+					"lemminx",
+					"pyright",
+					"typos_lsp",
+					"grammarly",
+				},
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -31,11 +55,10 @@ return {
 			lspconfig.typos_lsp.setup({})
 			lspconfig.grammarly.setup({})
 
-
-			  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
-	}
+	},
 }

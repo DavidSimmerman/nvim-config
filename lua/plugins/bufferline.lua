e24f815110e1831a27b569ca88326703a0b67e1d
David Simmerman <davidsimmermancs@gmail.com>
2024-05-20 00:54:05 -0400

    add bufferline sep style and pick commands

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,11 @@
 return {
 	"akinsho/bufferline.nvim",
-	config = function(_, opts)
-		require("bufferline").setup(opts)
+	config = function()
+		require("bufferline").setup({
+			options = {
+				separator_style = "slope",
+			},
+		})
 
 		vim.keymap.set("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle Pin" })
 		vim.keymap.set(
@@ -13,6 +17,8 @@ return {
 		vim.keymap.set("n", "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", { desc = "Delete Other Buffers" })
 		vim.keymap.set("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { desc = "Delete Buffers to the Right" })
 		vim.keymap.set("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { desc = "Delete Buffers to the Left" })
+		vim.keymap.set("n", "<leader>bb", "<Cmd>BufferLinePick<CR>", { desc = "Pick Buffer" })
+		vim.keymap.set("n", "<leader>bc", "<Cmd>BufferLinePickClose<CR>", { desc = "Pick Close Buffer" })
 		vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
 		vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
 		vim.keymap.set("n", "[b", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })

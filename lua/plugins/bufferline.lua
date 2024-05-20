51b704220e82ea564bca08ac776d54dff2299792
David Simmerman <davidsimmermancs@gmail.com>
2024-05-20 00:36:06 -0400

    add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+	"akinsho/bufferline.nvim",
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+
+		vim.keymap.set("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle Pin" })
+		vim.keymap.set(
+			"n",
+			"<leader>bP",
+			"<Cmd>BufferLineGroupClose ungrouped<CR>",
+			{ desc = "Delete Non-Pinned Buffers" }
+		)
+		vim.keymap.set("n", "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", { desc = "Delete Other Buffers" })
+		vim.keymap.set("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { desc = "Delete Buffers to the Right" })
+		vim.keymap.set("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { desc = "Delete Buffers to the Left" })
+		vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
+		vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+		vim.keymap.set("n", "[b", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
+		vim.keymap.set("n", "]b", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+	end,
+}

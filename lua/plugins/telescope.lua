61940c92c01d5ee06f8d79e9e6c282ec527b6bf5
David Simmerman <davidsimmermancs@gmail.com>
2024-05-20 23:42:58 -0400

    update telescope finder options

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,11 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup({
-				pickers = { find_files = { hidden = true }, oldfiles = { cwd_only = true } },
+
+				pickers = {
+					-- find_files = { hidden = true },
+					oldfiles = { cwd_only = true },
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
@@ -24,7 +28,12 @@ return {
 				},
 			})
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+			vim.keymap.set(
+				"n",
+				"<leader>p",
+				":lua require'telescope.builtin'.find_files({ file_ignore_patterns = {'node_modules','yarn.lock','.git/','.undo'}, hidden = true })<CR>",
+				{}
+			)
 			vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})

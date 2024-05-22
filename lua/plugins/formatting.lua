a826e24a7312276d9d3f523003c7e5c228836bfe
David Simmerman <davidsimmermancs@gmail.com>
2024-05-21 22:42:09 -0400

    add conform for formatting

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				vue = { "prettier" },
+				css = { "prettier" },
+				scss = { "prettier" },
+				less = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				jsonc = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				handlebars = { "prettier" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cF", function()
+			require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+		end, {})
+	end,
+}

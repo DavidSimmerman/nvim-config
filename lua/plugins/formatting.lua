9695523dccbe3189680b3c0f3eb1d2d7d13054da
David Simmerman <davidsimmermancs@gmail.com>
2024-06-01 14:13:27 -0400

    switch to conform

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,31 +1,46 @@
 return {
 	"stevearc/conform.nvim",
-	opts = {},
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				vue = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				less = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				jsonc = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				handlebars = { "prettier" },
+				["javascript"] = { "prettier" },
+				["javascriptreact"] = { "prettier" },
+				["typescript"] = { "prettier" },
+				["typescriptreact"] = { "prettier" },
+				["vue"] = { "prettier" },
+				["css"] = { "prettier" },
+				["scss"] = { "prettier" },
+				["less"] = { "prettier" },
+				["html"] = { "prettier" },
+				["json"] = { "prettier" },
+				["jsonc"] = { "prettier" },
+				["yaml"] = { "prettier" },
+				["markdown"] = { "prettier" },
+				["markdown.mdx"] = { "prettier" },
+				["graphql"] = { "prettier" },
+				["handlebars"] = { "prettier" },
+			},
+			-- format_on_save = {
+			-- 	-- These options will be passed to conform.format()
+			-- 	timeout_ms = 500,
+			-- 	lsp_fallback = true,
+			-- },
+			format = {
+				timeout_ms = 3000,
+				async = false, -- not recommended to change
+				quiet = false, -- not recommended to change
+				lsp_fallback = true, -- not recommended to change
 			},
 		})
 
-		vim.keymap.set("n", "<leader>cF", function()
-			require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+		vim.keymap.set("n", "<leader>cf", function()
+			require("conform").format()
 		end, {})
 	end,
 }

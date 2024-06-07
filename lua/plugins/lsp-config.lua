c17a57abfee354253ad14ed74988b62fc743647d
David Simmerman <davidsimmermancs@gmail.com>
2024-06-07 16:17:38 -0400

    change lsp sign column signs to icons

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -60,6 +60,12 @@ return {
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+			end
 		end,
 	},
 }
